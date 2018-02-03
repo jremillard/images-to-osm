5375d69c0f78aba9c153b0ada7ef7d6b8fb87b13
Jason Remillard <remillard.jason@gmail.com>
2018-02-02 22:35:51 -0500

    Write out GeoJSON file when extracting training data from OSM. The next rev of pipeline will handle any GeoJSON file rather than the custom CSV's.

--- getdatafromosm.py
+++ getdatafromosm.py
@@ -2,6 +2,10 @@ import sys
 import overpy
 import imagestoosm.config as cfg
 import os
+import shapely.geometry
+import shapely.wkt
+import shapely.ops
+import geojson
 
 api = overpy.Overpass()
 
@@ -16,26 +20,48 @@ def saveOsmData(query) :
         # "leisure=pitch,sport=" , don't use "-" char" in featureDirectoryName
         featureDirectoryName = way.tags.get("sport")
 
-        csvDirectoryName = os.path.join(cfg.rootOsmDir,featureDirectoryName)
-        if ( os.path.exists(csvDirectoryName) == False):
-            os.makedirs(csvDirectoryName)
+        outputDirectoryName = os.path.join(cfg.rootOsmDir,featureDirectoryName)
+        if ( os.path.exists(outputDirectoryName) == False):
+            os.makedirs(outputDirectoryName)
 
         if ( (featureDirectoryName in summary) == False) :
             summary[featureDirectoryName]  = 1
         else:     
             summary[featureDirectoryName] += 1
         
-        csvFilename = os.path.join(cfg.rootOsmDir,featureDirectoryName,str(way.id))
+        filenameBase= os.path.join(cfg.rootOsmDir,featureDirectoryName,str(way.id))
 
-        print("Name: %d %s %s" % ( way.id ,way.tags.get("name", ""),csvFilename))
+        #print("Name: %d %s %s" % ( way.id ,way.tags.get("name", ""),filenameBase))
 
-        with open("%s.csv" % (csvFilename), "wt") as text_file:
+        # leave the csv file for now, will delete when the script for the next
+        # stage is rewritten.
+        with open("%s.csv" % (filenameBase), "wt") as text_file:
             for node in way.nodes:
                 text_file.write("%0.7f\t%0.7f\n" % (node.lat, node.lon))
-    
 
-# fetch all ways and nodes
-queryMA = """[timeout:125];
+        with open("%s.GeoJSON" % (filenameBase), "wt") as text_file:
+
+            rawNodes = []
+            for node in way.nodes:
+                rawNodes.append( (node.lon, node.lat) )
+            
+            try:
+                geom = shapely.geometry.Polygon(rawNodes)
+
+                tags = way.tags
+                tags['wayOSMId'] = way.id
+
+                features =[]            
+                features.append( geojson.Feature(geometry=geom, properties=tags))
+
+                featureC = geojson.FeatureCollection(features)
+
+                text_file.write(geojson.dumps(featureC))
+            except Exception as e:
+                print(e)
+                    
+
+queryFull = """[timeout:125];
     ( 
         area[admin_level=4][boundary=administrative][name="Massachusetts"];         
         area[admin_level=4][boundary=administrative][name="New York"];         
@@ -53,7 +79,23 @@ queryMA = """[timeout:125];
     (._;>;);
     out body;
     """
-saveOsmData(queryMA)
+
+# fetch all ways and nodes
+queryMA = """[timeout:125];
+    ( 
+        area[admin_level=4][boundary=administrative][name="Massachusetts"];         
+    )->.searchArea;
+    (
+    way["sport"="baseball"]["leisure"="pitch"](area.searchArea);
+    way["sport"="tennis"]["leisure"="pitch"](area.searchArea);
+    way["sport"="soccer"]["leisure"="pitch"](area.searchArea);
+    way["sport"="american_football"]["leisure"="pitch"](area.searchArea);    
+    way["sport"="basketball"]["leisure"="pitch"](area.searchArea);    
+    );
+    (._;>;);
+    out body;
+    """
+saveOsmData(queryFull)
     
 # Other possible data to query
 #  - bridges
